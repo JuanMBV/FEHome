66e46e0885e7d4bf4ff039ac032699a224c4da63
Juan Buenrostro <juanmbv.31@gmail.com>
2024-07-19 21:39:14 -0600

    create project

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native-web';
+import { View } from 'react-native';
+import Main from './components/Main';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View className='flex-1 justify-center items-center'>
+        <StatusBar style="dark" />
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/(tabs)/about.js
@@ -0,0 +1,8 @@
+import { View, Text, Image} from "react-native";
+export default function About() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>About page</Text>
+    </View>
+  );
+}
