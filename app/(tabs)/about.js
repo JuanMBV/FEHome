13c90107f231a06efcfd6347c0607c6ebc3cb943
Juan Buenrostro <juanmbv.31@gmail.com>
2024-07-25 00:44:24 -0600

    chore: Remove unused Image import in about.js

--- app/(tabs)/about.js
+++ app/(tabs)/about.js
@@ -1,4 +1,4 @@
-import { View, Text, Image} from "react-native";
+import { View, Text } from "react-native";
 export default function About() {
   return (
     <View className="flex-1 justify-center items-center">
