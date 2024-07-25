a5a383bbeb76b349c3256601fbe75b4c62c1441c
Juan Buenrostro <juanmbv.31@gmail.com>
2024-07-25 00:45:36 -0600

    chore: Add NewHeroesCard component for displaying new heroes section

--- components/home/Main.jsx
+++ components/home/Main.jsx
@@ -1,11 +1,12 @@
 import { Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import NewHeroesCard from './NewHeroesCard';
 
 export default function Main() {
   const insets = useSafeAreaInsets();
   return (  
-    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom}}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center" style={{ paddingBottom: insets.bottom}}>
+      <NewHeroesCard />
     </View>
   );
 }
