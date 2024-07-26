470cd1cdf5a24ab27d0527aa1ff6f1eaeac1ad12
Juan Buenrostro <juanmbv.31@gmail.com>
2024-07-25 19:15:30 -0600

    chore: Update ImgRounded border thickness to 2 pixels

--- components/home/ImgRounded.jsx
+++ components/home/ImgRounded.jsx
@@ -7,7 +7,7 @@ const StyledPressable = styled(Pressable);
 export default function ImgRounded({src}) {
   return (
     <Link href={"/Hero"} asChild>
-      <StyledPressable className="mx-2 rounded-full border border-black">
+      <StyledPressable className="mx-2 rounded-full border-2 border-black">
         <Image 
           className="w-12 h-12 rounded-full"
           source={src}
